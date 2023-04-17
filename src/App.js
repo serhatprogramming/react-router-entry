384ff759b1bcbe44282257cc7c898a95a585c231
serhatprogramming <serhatprogramming@gmail.com>
2023-04-17 11:02:22 -0400

    routes and links added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
-function App() {
-  return <div>Test</div>;
-}
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const Home = () => (
+  <div>
+    <h2>TKTL notes app</h2>{" "}
+  </div>
+);
+
+const Notes = () => (
+  <div>
+    <h2>Notes</h2>
+  </div>
+);
+
+const Users = () => (
+  <div>
+    <h2>Users</h2>
+  </div>
+);
+
+const App = () => {
+  const padding = { padding: 5 };
+
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
